c27d5bd2e3c11e7b8bdb62364634888c1a5aa28f
SPMSPI <kislicin.artur@gmail.com>
2026-02-22 17:03:16 +0200

    ready

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for person in people:
+        obj = Person(person["name"], person["age"])
+        result.append(obj)
+    for i in range(len(result)):
+        one_dict = people[i]
+        p_obj = result[i]
+        if one_dict.get("wife"):
+            wife_name = one_dict["wife"]
+            p_obj.wife = Person.people[wife_name]
+
+        if dict.get("husband"):
+            husband_name = dict["husband"]
+            p_obj.husband = Person.people[husband_name]
+    return result
